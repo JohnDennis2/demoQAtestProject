ab95899aac69703eb50668d9392de16edff69ac7
John D <johnd@Johns-MBP.attlocal.net>
2024-09-02 14:12:28 -0500

    new test

--- tests/newTest.spec.ts
+++ tests/newTest.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test'
 
-test('basic test', async({page}) => {
+test.beforeEach('navigate to page', async({page}) => {
     await page.goto('https://demoqa.com/')
 })
 
