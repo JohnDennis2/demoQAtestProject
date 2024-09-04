0acf55cf78b3ccdc7d1b728645ab4eb27d9b3623
John D <johnd@Johns-MBP.attlocal.net>
2024-09-04 14:01:59 -0500

    textbox check

--- tests/newTest.spec.ts
+++ tests/newTest.spec.ts
@@ -4,3 +4,7 @@ test.beforeEach('navigate to page', async({page}) => {
     await page.goto('https://demoqa.com/')
 })
 
+test('Textbox', async ({page}) => {
+    const textBoxform = page.locator('avatar mr-sm-2')
+    await textBoxform.click()
+})
